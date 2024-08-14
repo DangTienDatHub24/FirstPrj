26dd8a32a33114f0c2aa3910d405461c20c43757
DangTienDatHub24 <23683961.dat@student.iuh.edu.vn>
2024-08-15 01:14:28 +0700

    First commit

---
+++ src/Wellcome.java
@@ -0,0 +1,6 @@
+public class Wellcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Dang Tien Dat");
+        System.out.println("Welcome to the DHKTPM19B");
+    }
+}
